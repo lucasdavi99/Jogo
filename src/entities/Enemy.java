e79b3c7721b1f77bc13f971368253d766388b98d
Lucas Davi <95988791+lucasdavi99@users.noreply.github.com>
2024-03-08 12:04:33 -0300

    IA nos inimigos

--- src/entities/Enemy.java
+++ src/entities/Enemy.java
@@ -1,9 +1,28 @@
 package entities;
 
+import main.Game;
+
 import java.awt.image.BufferedImage;
 
 public class Enemy extends Entity{
+
+    private int speed = 1;
+
     public Enemy(int x, int y, int width, int height, BufferedImage sprite) {
         super(x, y, width, height, sprite);
+    }
+
+    public void tick() {
+        if (x < Game.player.getX() && Game.world.isFree((x+speed), this.getY())) {
+            x+=speed;
+        } else if (x > Game.player.getX() && Game.world.isFree((x-speed), this.getY())) {
+            x-=speed;
+        }
+
+        if (y < Game.player.getY() && Game.world.isFree(this.getX(), (y+speed))) {
+            y+=speed;
+        } else if (y > Game.player.getY() && Game.world.isFree(this.getX(), (y-speed))) {
+            y-=speed;
+        }
     }
 }
